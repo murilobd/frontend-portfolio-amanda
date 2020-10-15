d3b6ad874b4d6c11f56fefdab3bbb2e0bbdefa2d
Murilo Boareto Delefrate <murilobd@gmail.com>
2020-10-15 12:26:16 +0200

    - google analytics

--- src/helpers/googleAnalytics.js
+++ src/helpers/googleAnalytics.js
@@ -1,10 +1,11 @@
 export const sendPageView = (page_title) => {
 	console.log(page_title);
-	window.gtag("event", "page_view", {
-		page_title,
-		page_location: window.location.href,
-		page_path: window.location.pathname,
-	});
+	window.gtag &&
+		window.gtag("event", "page_view", {
+			page_title,
+			page_location: window.location.href,
+			page_path: window.location.pathname,
+		});
 };
 
 export default sendPageView;
